20b814060fac2dc41c97b67963dda416491e6ff1
Jeroen Smink <jeroensmink98@gmail.com>
2025-09-30 20:36:08 +0200

    Update project configuration to use @sveltejs/adapter-static, enhance SvelteKit settings for prerendering, and add layout file for default prerendering. Update package.json and pnpm-lock.yaml to include new adapter dependency. Modify GitHub Actions workflow to cache pnpm and streamline setup process.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,20 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
-};
+const dev = process.argv.includes('dev');
+const repoName = 'biblgpt'; 
 
-export default config;
+export default {
+  kit: {
+    adapter: adapter({
+      fallback: '404.html'
+    }),
+    paths: {
+      base: dev ? '' : `/${repoName}`,
+    },
+    prerender: {
+      default: true
+    }
+  },
+  preprocess: vitePreprocess()
+};
